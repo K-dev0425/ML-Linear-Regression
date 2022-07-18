7e74f898299dcb99b9cec2c0d30766c7df8e8b3e
K-dev0425 <kante0425@outlook.com>
2022-07-18 21:22:55 +0800

    logistic - creditScoring

--- LogisticRegressionCreditScoring.py
+++ LogisticRegressionCreditScoring.py
@@ -1,2 +1,20 @@
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+
+credit_data = pd.read_csv("credit_data.csv")
+
+features = credit_data[["income", "age", "loan"]]
+target = credit_data.default
+
+feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.3, train_size=0.7)
+
+model = LogisticRegression()
+model.fit = model.fit(feature_train, target_train)
+
+predictions = model.fit.predict(feature_test)
+
+print(confusion_matrix(target_test, predictions))
+print(accuracy_score(target_test, predictions))
