1c77a79dd489bc24060c8547c5059ceeea0d562c
K-dev0425 <kante0425@outlook.com>
2022-07-20 21:52:05 +0800

    Cross Validation

---
+++ LogisticRegressionCreditCrossValid.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import cross_validate
+
+credit_data = pd.read_csv("credit_data.csv")
+
+features = credit_data[["income", "age", "loan"]]
+target = credit_data.default
+
+X = np.array(features).reshape(-1 ,3)
+y = np.array(target)
+
+model = LogisticRegression()
+predicted = cross_validate(model, X, y, cv=100)
+
+print(np.mean(predicted['test_score']))
--- LogisticRegressionCreditScoring.py
+++ LogisticRegressionCreditScoring.py
@@ -9,7 +9,7 @@ credit_data = pd.read_csv("credit_data.csv")
 features = credit_data[["income", "age", "loan"]]
 target = credit_data.default
 
-feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.3, train_size=0.7)
+feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.3)
 
 model = LogisticRegression()
 model.fit = model.fit(feature_train, target_train)
