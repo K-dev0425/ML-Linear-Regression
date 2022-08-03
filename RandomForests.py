c40374f5c66e1cf33f954bab956a1241080c1759
K-dev0425 <kante0425@outlook.com>
2022-08-03 09:58:00 +0800

    SVM, RandomForests

---
+++ RandomForests.py
@@ -0,0 +1,6 @@
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn import datasets
+
---
+++ SVM.py
@@ -0,0 +1,10 @@
+from sklearn.svm import SVC
+
+classifier = SVC(kernel='linear')
+
+training_points = [[1, 2], [1, 5], [2, 2], [7, 5], [9, 4], [8, 2]]
+labels = [1, 1, 1, 0, 0, 0]
+
+classifier.fit(training_points, labels)
+
+print(classifier.support_vectors_)
